685c1c762116c9bda3ecfd3da0d59f0028fce25e
GiacAlf <giacomoalfani@gmail.com>
2024-09-04 17:11:29 +0200

    modified in english some file e setup bash done

--- DevOps_Pipeline_Kubernetes/setup_cluster_and_jenkins.sh
+++ DevOps_Pipeline_Kubernetes/setup_cluster_and_jenkins.sh
@@ -64,3 +64,28 @@ sudo kubectl apply -f $JENKINS_SA_BINDING
 check_command "kubectl apply -f $JENKINS_SA_BINDING"
 
 echo "Installazione di Jenkins completata con successo!"
+
+# stampe utili
+echo "Recupero IP e porta dell'applicazione e IP di Jenkins..."
+# Recupera l'IP del control plane
+CONTROL_PLANE_IP=$(sudo kubectl get nodes -o wide | grep control-plane | awk '{print $6}')
+
+# Recupera la porta del servizio clusterdeplo
+CLUSTERDEPLO_PORT=$(sudo kubectl get svc clusterdeplo -o wide | awk '{if(NR>1) print $5}' | cut -d':' -f2 | cut -d'/' -f1)
+
+# Combina IP e porta
+APP_IP_PORT="${CONTROL_PLANE_IP}:${CLUSTERDEPLO_PORT}"
+
+# Stampa l'IP e la porta dell'applicazione
+echo "The application IP and port: $APP_IP_PORT"
+echo "The Jenkins IP is: 172.18.0.2:30000"
+
+# Recupera la password di Jenkins
+echo "Recupero password Jenkins..."
+JENKINS_PASSWORD=$(sudo kubectl logs $(sudo kubectl get pods -o wide | grep jenkins | awk '{print $1}') | awk '/proceed to installation:/{getline; getline; print}')
+echo "Jenkins password: $JENKINS_PASSWORD"
+
+# Recupera il token per il service account jenkins-sa
+echo "Creazione token jenkins per kubectl..."
+JENKINS_SA_TOKEN=$(sudo kubectl create token jenkins-sa)
+echo "Jenkins service account token for Kubectl: $JENKINS_SA_TOKEN"
